9f4476c7ef2cb8cee62db8cbf5326377c2fc7fd7
Neville <otiendeneville@gamil.com>
2026-05-08 08:58:35 +0300

    adding a button

--- Desktop/php-laravel/lms/resources/views/ideas.blade.php
+++ Desktop/php-laravel/lms/resources/views/ideas.blade.php
@@ -7,11 +7,16 @@
   
   <form action="">
    <div class="col-span-full">
-      <label for="idea" class="block text-sm/6 font-medium text-white">Idea</label>
+      <label for="idea" class="block text-sm/6 font-medium text-white">New Idea</label>
       <div class="mt-2">
          <textarea  id="idea" name="idea"  cols="30" rows="3" class="block w-full rounded-md bg-white/5 px-3"></textarea>
       </div>
-           <p class="mt- 3 text-sm/6 text-white-400">Write a few sentence about yourself</p>
+           <p class="mt- 3 text-sm/6 text-white-400">You have an idea want to save for later.</p>
+   </div>
+   <div class="mt-6 flex items-center gap-x-6">
+      <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+
+
    </div>
   </form>
 
